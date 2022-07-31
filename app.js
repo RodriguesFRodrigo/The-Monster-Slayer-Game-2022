93b5956cb3fa48ce08e433f6caedcb214ff88bb2
Rodrigo Rodrigues <rodrigo.rodrigues@compasso.com.br>
2022-07-31 00:50:39 -0300

    feature/51: Setup & First Methods

---
+++ app.js
@@ -0,0 +1,29 @@
+// JS
+function getRandomValue(maxValue, mimValue) {
+    return Math.floor((Math.random() * (maxValue - mimValue)) + mimValue);
+}
+
+// Vue
+const app = Vue.createApp({
+    data() {
+        return {
+            playerHealth: 100,
+            monsterHealth: 100,
+        };
+    },
+
+    methods: {
+        attackMonster() {
+            const attackMonster = getRandomValue(12, 5);
+            this.monsterHealth -= attackMonster;
+            this.attackPlayer();
+        },
+
+        attackPlayer() {
+            const attackPlayer = getRandomValue(15, 8);
+            this.playerHealth -= attackPlayer;
+        }
+    }
+});
+
+app.mount("#game");
